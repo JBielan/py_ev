65cb28a8e0a5c9e15423cac32268c7e908c4e815
Jakub Bielan <31440616+jbielan@users.noreply.github.com>
2019-05-29 20:00:29 +0200

    add equity tests

--- py_ev/py_ev.py
+++ py_ev/py_ev.py
@@ -272,9 +272,3 @@ class Evaluator:
         # print('Draw: {:.2f}%'.format(percentage_draw))
 
         return percentage_1, percentage_2, percentage_draw
-
-# For tests:
-
-# ev = Evaluator()
-# print(ev.equity(1000000, [(14, 4), (14, 2)], [(8, 3), (9, 3)]))
-# print(ev.evaluate( [(3, 3), (3, 2)], [(2, 2), (11, 3), (3, 4), (10, 2), (7, 3)] ))
